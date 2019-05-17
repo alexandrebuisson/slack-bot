dbdcce9a97a5a8daad78d56b44a75aaaf5c79713
alexandrebuisson <buisson.alexandre@orange.fr>
2019-05-17 10:13:15 +0200

    return random memes from dankmemes subreddit

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 const { Router } = require('express');
+const randomPuppy = require('random-puppy');
+const snoowrap = require('snoowrap');
 
 const router = Router();
 
-/* GET index page. */
-router.get('/hotmemes/:posturl', (req, res) => {
-  const posturl = req.params
-  res.json({
-    title: posturl
-  });
+router.get('/randommeme', function(request, response) {
+  const event = randomPuppy.all("dankmemes");
+  event.on('data', url => response.send(url))
 });
 
 module.exports = router;
