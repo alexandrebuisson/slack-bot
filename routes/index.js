b3358f04340b669abaf04064d47f21a69a889f02
alexandrebuisson <buisson.alexandre@orange.fr>
2019-05-17 12:03:11 +0200

    retour ancien commit

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,4 @@ router.post('/randommeme', function(request, response) {
   event.on('data', url => response.send(url))
 });
 
-router.post('/memes/:subreddit', function(request, response) {
-  const subreddit = request.params;
-  const event = randomPuppy.all(subreddit);
-  event.on('data', url => response.send(url))
-});
-
 module.exports = router;
