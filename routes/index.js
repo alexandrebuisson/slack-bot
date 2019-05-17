5c35795048aef64484053b99b64fcba73af84967
alexandrebuisson <buisson.alexandre@orange.fr>
2019-05-17 18:18:12 +0200

    bot v2

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,25 @@
 const { Router } = require('express');
 const randomPuppy = require('random-puppy');
-const snoowrap = require('snoowrap');
+
 
 const router = Router();
 
-router.post('/randommeme', function(request, response) {
+
+
+router.post('/randommeme', function(req, res) {
   const event = randomPuppy.all("dankmemes");
-  event.on('data', url => response.send(url))
+  event.on('data', url => res.json({
+      response_type: 'in_channel',
+      channel: 'CHSSBBX5F',
+      text: 'Voilà ton meme :',
+      attachments: [
+        {
+          "fallback": "Required plain-text summary of the attachment.",
+          "color": "#36a64f",
+          "image_url": url
+        }
+      ]
+    }))
 });
 
 module.exports = router;
