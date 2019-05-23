2700cae9f1e4a6c0d63bf7c8e430ecd0bcff0aac
alexandrebuisson <buisson.alexandre@orange.fr>
2019-05-23 14:10:29 +0200

    add /joke generate random jokes

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.post('/randommeme', function(req, res) {
     }))
 });
 
-router.post('/randomchucknorris', function(req, res, next) {
+router.post('/randomchucknorris', function(req, res) {
   request("https://api.chucknorris.io/jokes/random", function(err, response, body) {
     if(err || response.statusCode !== 200) {
       return  res.json({response_type: 'in_channel', channel: 'CHSSBBX5F', text: "Erreur :/ merci de contacter @Alex'"})
@@ -37,5 +37,23 @@ router.post('/randomchucknorris', function(req, res, next) {
     
   })
 });
+
+router.post('/randomjoke', function(req, res) {
+
+  const rnd = Math.floor(Math.random() *  115);
+
+  request(`https://bridge.buddyweb.fr/api/blagues/blagues/${rnd}`, function(err, response, body) {
+    if (err || response.statusCode !== 200) {
+      return res.json({response_type: 'in_channel', channel: 'CHSSBBX5F', text: "Erreur :/ merci de contacter @Alex'"})
+    }
+
+    res.json({
+      response_type: 'in_channel',
+      channel: 'CHSSBBX5F',
+      text: `${body}`
+    })
+
+  })
+})
 
 module.exports = router;
